6dd8a1faaad1621e6b909afcd04d8250d7c9cda8
sathish <sathishk@degreed.com>
2025-03-18 15:01:25 +0530

    First run

---
+++ llm_qa_giskard.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cba1b2a97a2434b7cb8a108d6aa8db7676ba82ef1d659c290927e35f086a3522
+size 2055
---
+++ test_llm.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:03d15fdd49eb9b67fa81006395dcc3f8d100adedd611a3a025a4cbbbacb5df25
+size 433
